076d22a1c8d4d7a0c4fd9ec0271c22ca2d988556
Drew Letcher <drew@drewletcher.net>
2019-10-03 00:07:45 -0500

    server startup sequence; shared logger

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const Server = require('./lib/server');
 const app = require('./lib/app');
 const authorize = require('./lib/authorize');
 const roles = require('./lib/roles');
+const logger = require('./lib/logger');
 
 exports = module.exports = Server;
 
 exports.app = app;
 exports.authorize = authorize;
 exports.roles = roles;
+exports.logger = logger;
--- test/main.js
+++ test/main.js
@@ -5,12 +5,8 @@
 
 const StorageNode = require('../index');
 const routes = require('./routes');
-const logger = require('../lib/logger');
 
-logger.verbose('starting');
-logger.verbose('Adding route: /test');
 StorageNode.start({
   routerPath: '/test',
   router: routes
 });
-logger.verbose('started');
