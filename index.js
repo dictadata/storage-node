c007edeeab55a52b45694aff3457406967c66c85
Drew Letcher <drew@drewletcher.net>
2019-10-13 09:57:07 -0500

    startup mechanism

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 const Server = require('./lib/server');
 const app = require('./lib/app');
+const startup = require('./lib/startup');
 const authorize = require('./lib/authorize');
 const roles = require('./lib/roles');
 const logger = require('./lib/logger');
@@ -14,4 +15,5 @@ exports = module.exports = Server;
 exports.app = app;
 exports.authorize = authorize;
 exports.roles = roles;
+exports.startup = startup;
 exports.logger = logger;
