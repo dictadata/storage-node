d54e3d94282d86006d0c495138f66108fe7dbaba
Drew Letcher <drew@drewletcher.net>
2020-03-29 23:09:48 -0500

    version 1.1.0 testing

---
+++ local mysql storage_node.sql
@@ -0,0 +1,18 @@
+SHOW TABLES;
+
+SELECT * FROM foo_schema;
+SELECT * FROM foo_transfer;
+SELECT * FROM foo_transfer_3;
+SELECT * FROM foo_dbtransform;
+SELECT * FROM rest_forecast;
+
+DELETE FROM foo_schema;
+DELETE FROM foo_transfer;
+DELETE FROM foo_dbtransform;
+DELETE FROM rest_forecast;
+
+DROP TABLE foo_schema;
+DROP TABLE foo_transfer;
+DROP TABLE foo_transfer_3;
+DROP TABLE foo_dbtransform;
+DROP TABLE rest_forecast;
--- test/sql/local mysql Session.sql
+++
@@ -1,5 +0,0 @@
-show tables;
-
-select * from foo_schema where Foo = 'first';
-
-drop table foo_schema;
