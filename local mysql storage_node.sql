fe625fc4a00ff4ed8a74e190d93be14a1c3da43f
Drew Letcher <drew@drewletcher.net>
2020-04-04 17:41:34 -0500

    test updates

--- local mysql storage_node.sql
+++ local mysql storage_node.sql
@@ -4,15 +4,15 @@ SELECT * FROM foo_schema;
 SELECT * FROM foo_transfer;
 SELECT * FROM foo_transfer_3;
 SELECT * FROM foo_dbtransform;
-SELECT * FROM rest_forecast;
+SELECT * FROM weather_forecast;
 
 DELETE FROM foo_schema;
 DELETE FROM foo_transfer;
 DELETE FROM foo_dbtransform;
-DELETE FROM rest_forecast;
+DELETE FROM weather_forecast;
 
 DROP TABLE foo_schema;
 DROP TABLE foo_transfer;
 DROP TABLE foo_transfer_3;
 DROP TABLE foo_dbtransform;
-DROP TABLE rest_forecast;
+DROP TABLE weather_forecast;
