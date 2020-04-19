db708f8f6ed573c2fbdcd018c99db2aee3225418
Drew Letcher <drew@drewletcher.net>
2020-04-18 23:44:21 -0500

    testing updates

--- local mysql storage_node.sql
+++ local mysql storage_node.sql
@@ -1,6 +1,9 @@
 SHOW TABLES;
 DESCRIBE foo_schema;
 DESCRIBE foo_transfer;
+DESCRIBE foo_transform;
+DESCRIBE foo_dbtransform;
+DESCRIBE weather_forecast;
 DESCRIBE foo_schema_etl;
 DESCRIBE foo_schema_etl2;
 
@@ -9,12 +12,16 @@ SELECT * FROM foo_transfer;
 SELECT * FROM foo_transform;
 SELECT * FROM foo_dbtransform;
 SELECT * FROM weather_forecast;
+SELECT * FROM foo_schema_etl;
+SELECT * FROM foo_schema_etl2;
 
 DELETE FROM foo_schema;
 DELETE FROM foo_transfer;
 DELETE FROM foo_transform;
 DELETE FROM foo_dbtransform;
 DELETE FROM weather_forecast;
+DELETE TABLE foo_schema_etl;
+DELETE TABLE foo_schema_etl2;
 
 DROP TABLE foo_schema;
 DROP TABLE foo_transfer;
