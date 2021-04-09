f8355ca35562465a50b25002845f9f37376c5954
drewlab <drew@dictadata.org>
2021-04-09 12:27:26 -0500

    set main to storage/index.js

---
+++ storage/index.js
@@ -0,0 +1,26 @@
+/**
+ * @dictadata/storage-node
+ */
+"use strict";
+
+const server = require('./node/server');
+const app = require('./node/app');
+
+const authenticate = require('./node/authenticate');
+const authorize = require('./node/authorize');
+const roles = require('./node/roles');
+const Account = require('./node/account');
+
+const startup = require('./node/startup');
+const logger = require('./utils/logger');
+
+exports = module.exports = server;
+exports.app = app;
+
+exports.authenticate = authenticate;
+exports.authorize = authorize;
+exports.roles = roles;
+exports.Account = Account;
+
+exports.startup = startup;
+exports.logger = logger;  // same Winston instance as storage-junctions' logger
